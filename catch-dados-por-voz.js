5d9a2952012b4a4c20ad40ed6ca2a84f4bd4d87c
pedrosantos4 <pedro.santos4@zazvendas.com.br>
2022-06-09 17:35:16 -0300

    Update catch-dados-por-voz.js

--- catch-dados-por-voz.js
+++ catch-dados-por-voz.js
@@ -1,15 +1,13 @@
 var lista = ["loja", "estabelecimento", "lojinha", "barzinhos"];
-var text = "";
-var verificapj = vars.voz.match(/\d+/g);
+vars.nomeLoja = "";
 vars.voz = vars.voz.replace("mil contra", "0001").replace("meia", "6").replace("meia dúzia", "6").replace("meia duzia", "6");
 var regexCPF = new RegExp('([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})');
 var regexCNPJ = new RegExp('[0-9]{2}\.?[0-9]{3}\.?[0-9]{3}\-?[ ]\/?[0-9]{4}\-?[ ]\-?[0-9]{2}');
 var cpf = vars.voz.match(regexCPF);
 var cnpj = vars.voz.match(regexCNPJ).join().replace(/ /g, "");
-var validaCNPJ = cnpj.match(/[0-9]{2}\.?[0-9]{3}\.?[0-9]{3}\/?[0-9]{4}\-?[0-9]{2}/g);
+vars.cpf = cpf[0];
 lista.sort().forEach(estabelecimento => {
     if (vars.voz.indexOf(estabelecimento) >= 0) {
-        text = vars.voz.substring(vars.voz.indexOf(estabelecimento))
+        vars.nomeLoja = vars.voz.substring(vars.voz.indexOf(estabelecimento));
     }
 });
-console.log(text, '\ncnpj: ', cnpj, '\ncpf: ', cpf);
