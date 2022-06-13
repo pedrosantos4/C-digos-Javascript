c1640e9ccc01ed3e3153e8c02e4578e46e58ab30
pedrosantos4 <pedro.santos4@zazvendas.com.br>
2022-06-13 09:38:14 -0300

    Update catch-dados-por-voz.js

--- catch-dados-por-voz.js
+++ catch-dados-por-voz.js
@@ -1,12 +1,14 @@
-var lista = ["loja", "estabelecimento", "lojinha", "barzinhos"];
+var listaEstabelecimentos = ["loja", "estabelecimento", "lojinha", "barzinhos", "Salão de Beleza", "salao de beleza", "salao de belesa", "çalao de beleza", "mercadinho", "mercado", "super mercado", "supermercado", "hipermercado"];
 vars.nomeLoja = "";
 vars.voz = vars.voz.replace("mil contra", "0001").replace("meia", "6").replace("meia dúzia", "6").replace("meia duzia", "6");
+//REGEX CPF
 var regexCPF = new RegExp('([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})');
+//REGEX CNPJ
 var regexCNPJ = new RegExp('[0-9]{2}\.?[0-9]{3}\.?[0-9]{3}\-?[ ]\/?[0-9]{4}\-?[ ]\-?[0-9]{2}');
 var cpf = vars.voz.match(regexCPF);
 var cnpj = vars.voz.match(regexCNPJ).join().replace(/ /g, "");
 vars.cpf = cpf[0];
-lista.sort().forEach(estabelecimento => {
+listaEstabelecimentos.sort().forEach(estabelecimento => {
     if (vars.voz.indexOf(estabelecimento) >= 0) {
         vars.nomeLoja = vars.voz.substring(vars.voz.indexOf(estabelecimento));
     }
