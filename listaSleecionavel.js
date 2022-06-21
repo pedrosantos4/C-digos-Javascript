6c51cdf663fdac35f49a8072c9fd825769d33e98
pedrosantos4 <pedro.santos4@zazvendas.com.br>
2022-06-21 10:13:03 -0300

    ok

--- listaSleecionavel.js
+++ listaSleecionavel.js
@@ -227,4 +227,15 @@ for (i = 0; i < vars.userData1.lista.length - 1; i++) {
       msgTxt += `*${i+1}* - *Estabelecimento:* ${e.baseProspeccao.nome}\n*Endereço:* ${e.baseProspeccao.logradouro} *Nº:* ${e.baseProspeccao.numero}\n\n`;
     }
   }
-  msg(`Qual dos estabelecimentos abaixo iremos visitar?\n\n${msgTxt}\n\n*0* - Ver mais`,'text');
+  msg(`Qual dos estabelecimentos abaixo iremos visitar?\n\n${msgTxt}\n*0* - Ver mais\n\nDigite o *NÚMERO* correspondente.`,'text');
+
+
+
+
+  msgTxt = '';
+  for (i = 0; i < vars.userData1.lista.length - 1; i++) {
+      e = (vars.userData1.lista[i]);
+      entity(encodeURIComponent(JSON.stringify(e)), (i+1).toString(), `${i+1} - ${e.baseProspeccao.nome} ${e.baseProspeccao.cnpjCompleto} ${e.baseProspeccao.logradouro} ${e.baseProspeccao.razaoSocial} ${e.baseProspeccao.bairro} ${e.baseProspeccao.numero}`);
+        msgTxt += `*${i+1}* - *Estabelecimento:* ${e.baseProspeccao.nome}\n*Endereço:* ${e.baseProspeccao.logradouro} *Nº:* ${e.baseProspeccao.numero}\n\n`;
+    }
+    msg(`Qual dos estabelecimentos abaixo iremos visitar?\n\n${msgTxt}\nDigite o *NÚMERO* correspondente.`,'text');
