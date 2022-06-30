20a8e492157dadf99b21ffb1206bd2fd17e45287
pedrosantos4 <pedro.santos4@zazvendas.com.br>
2022-06-30 20:43:05 -0300

    Update FiltraDataNaSemana.js

--- FiltraDataNaSemana.js
+++ FiltraDataNaSemana.js
@@ -1,18 +1,3 @@
-const getLastDay = (data, day) => {
-    while(data.getDay() != day){
-        data.setDate(data.getDate() - 1);
-    }
-    return data;
-};
-var data = new Date();
-data.setDate(data.getDate() - 1);
-data = getLastDay(data, 0);
-vars.dataFim = data;
-data = getLastDay(data, 1);
-vars.dataInicio = data;
-vars.dataFim = vars.dataFim.match(/([2022]{4}+[-]+[0-9]{2}+[-]+[0-9]{2})/g);
-vars.dataInicio = vars.dataInicio.match(/([2022]{4}+[-]+[0-9]{2}+[-]+[0-9]{2})/g); */
-
 vars = {};
 const getLastDay = (data, day) => {
     while(data.getDay() != day){
